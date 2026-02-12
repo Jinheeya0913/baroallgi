1916d364dbd8e285af161c1874c1e66a8563f6bd
jheeya <wehan0913@gmail.com>
2026-02-12 16:16:34 +0900

    1. AppBar 배경색 튀는 현상 해결 2. Drawer 옵션 활성

--- lib/core/ui/layout/DefaultPageLayout.dart
+++ lib/core/ui/layout/DefaultPageLayout.dart
@@ -1,3 +1,4 @@
+import 'package:baroallgi/core/ui/widgets/BaseDrawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -41,7 +42,7 @@ class DefaultLayout extends HookConsumerWidget {
 
     return Scaffold(
       //resizeToAvoidBottomInset : false,
-      drawer: null, // 드라우러 사용하기 전까지는 null 처리
+      drawer: useDrawer == true ? renderDrawer() : null,
       // const UserDrawer() : null,
       appBar: useAppBar ? _buildAppBar(context) : null,
       body: GestureDetector(
@@ -101,9 +102,14 @@ class DefaultLayout extends HookConsumerWidget {
       )
           : null,
       backgroundColor: Colors.white,
+      surfaceTintColor: Colors.white,
       centerTitle: true,
       elevation: 0,
-      toolbarHeight: 100,
+      toolbarHeight: 80,
     );
+  }
+
+  BaseDrawer renderDrawer() {
+    return BaseDrawer();
   }
 }
