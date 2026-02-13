6670e610f35014326d8bf2095750685692884395
jheeya <wehan0913@gmail.com>
2026-02-13 13:53:07 +0900

    report 페이지 생성(임시)

--- lib/features/report/presentation/report_page.dart
+++ lib/features/report/presentation/report_page.dart
@@ -1,3 +1,5 @@
+import 'package:baroallgi/core/const/const_size.dart';
+import 'package:baroallgi/core/ui/widgets/BaseTextField.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
@@ -13,41 +15,82 @@ class ReportPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final _controller = useMemoized(() => QuillController.basic());
+
     /// final _controller = QuillController.basic();
     /// => build가 실행될 대마다 새로운 컨트롤러가 생기므로
     /// 글을 쓰다가 화면이 리빌딩 될 경우 작성하던 내용이 초기화 될 위험이 있음
     /// userMemoized : 객체 생성 결과를 캐싱. 복잡하거나 비용이 큰 객체에 용이
+
+    // 툴바 보이기/숨기기
+    final isToolbarVisible = useState(true);
 
     return DefaultLayout(
       title: Text('작성'),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          QuillSimpleToolbar(
-            controller: _controller,
-            config: QuillSimpleToolbarConfig(
-              showUndo: false,
-              showRedo: false ,
-              showFontFamily: false,
-              showCodeBlock: false,
-              showListBullets: false,
-              showSuperscript: false,
-              showSubscript: false,
-              showClearFormat: false,
-              showInlineCode: false,
-              embedButtons: FlutterQuillEmbeds.toolbarButtons(
-                imageButtonOptions: QuillToolbarImageButtonOptions(),
-                videoButtonOptions: null,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('제목', style: TextStyle(fontSize: CNST_FONT_SIZE_BIG)),
+              SizedBox(width: CNST_SIZE_NORMAL,),
+              Expanded(
+                child: BaseTextField(
+                  contentPadding: 4.0,
+                  useBorder: false,
+                  style: TextStyle(fontSize: CNST_FONT_SIZE_NORMAL),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                child: IconButton(
+                  icon: Icon(
+                    isToolbarVisible.value
+                        ? Icons.keyboard_arrow_up
+                        : Icons.format_paint,
+                  ),
+                  onPressed: () =>
+                      isToolbarVisible.value = !isToolbarVisible.value,
+                  tooltip: '툴바 토글',
+                ),
+              ),
+            ],
+          ),
+          if (isToolbarVisible.value) ...[Divider()],
+          AnimatedCrossFade(
+            firstChild: QuillSimpleToolbar(
+              controller: _controller,
+              config: QuillSimpleToolbarConfig(
+                showUndo: false,
+                showRedo: false,
+                showFontFamily: false,
+                showCodeBlock: false,
+                showListBullets: false,
+                showSuperscript: false,
+                showSubscript: false,
+                showClearFormat: false,
+                showInlineCode: false,
+                embedButtons: FlutterQuillEmbeds.toolbarButtons(
+                  imageButtonOptions: QuillToolbarImageButtonOptions(),
+                  videoButtonOptions: null,
+                ),
               ),
             ),
+            secondChild: SizedBox.shrink(),
+            crossFadeState: isToolbarVisible.value
+                ? CrossFadeState.showFirst
+                : CrossFadeState.showSecond,
+            duration: const Duration(milliseconds: 200),
           ),
+          Divider(),
           Expanded(
             child: QuillEditor.basic(
               controller: _controller,
               config: QuillEditorConfig(
-                  embedBuilders: kIsWeb ? FlutterQuillEmbeds.editorWebBuilders() : FlutterQuillEmbeds.editorBuilders(),
+                embedBuilders: kIsWeb
+                    ? FlutterQuillEmbeds.editorWebBuilders()
+                    : FlutterQuillEmbeds.editorBuilders(),
               ),
             ),
           ),
