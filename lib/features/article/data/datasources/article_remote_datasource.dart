19c1a428a95a30d5006b2d0a59825c137f367274
jheeya <wehan0913@gmail.com>
2026-03-03 15:54:31 +0900

    article 목록 조회 기능 추가

--- lib/features/article/data/datasources/article_remote_datasource.dart
+++ lib/features/article/data/datasources/article_remote_datasource.dart
@@ -1,8 +1,7 @@
 import 'package:baroallgi/features/article/models/article_common_model.dart';
 import 'package:baroallgi/features/article/models/article_model.dart';
-import 'package:baroallgi/features/article/models/article_post_model.dart';
+import 'package:baroallgi/core/enum/article_type_enum.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_localization/script/upgrade_all_dependencies.dart';
 
 class ArticleDatasource {
   final FirebaseFirestore _firestore;
@@ -14,7 +13,6 @@ class ArticleDatasource {
 
   CollectionReference get articleCollection =>
       _firestore.collection(_articlePath);
-
 
   Future<void> saveArticleBatch({
     required ArticleModel mainInfo,
@@ -28,11 +26,9 @@ class ArticleDatasource {
     final mainDocRef = _firestore.collection(_articlePath).doc(id);
     final detailDocRef = mainDocRef.collection(_detailPath).doc(id);
 
-
     // 로그 출력
     print('rlog :: mainInfo.toJson :: ${mainInfo.toJson()}');
     print('rlog :: detailInfo.toJson :: ${detailInfo.toJson()}');
-
 
     // 정보 저장
     batch.set(mainDocRef, mainInfo.toJson());
@@ -41,7 +37,44 @@ class ArticleDatasource {
     await batch.commit();
   }
 
-  String getGenerateId(){
+  // 페이지네이션이 적용된 리스트 검색
+  Future<QuerySnapshot<Map<String, dynamic>>> selectArticleList({
+    String? keyword,
+    ArticleSelectType? selectType,
+    DocumentSnapshot? lastDocument,
+    int limit = 10,
+  }) async {
+    Query<Map<String, dynamic>> query = _firestore.collection(_articlePath);
+
+    if (keyword != null && keyword.isNotEmpty) {
+      String field;
+
+      switch (selectType) {
+        case ArticleSelectType.TITLE:
+          field = 'title';
+          break;
+        case ArticleSelectType.AUTHOR:
+          field = 'authorName';
+          break;
+        default:
+          field = 'title';
+      }
+      query = query
+          .where(field, isGreaterThanOrEqualTo: keyword)
+          .where(field, isLessThan: '${keyword}\uf8ff')
+          .orderBy(field);
+    } else {
+      query = query.orderBy('createdAt', descending: true);
+    }
+
+    if (lastDocument != null) {
+      query = query.startAfterDocument(lastDocument);
+    }
+
+    return query.limit(limit).get();
+  }
+
+  String getGenerateId() {
     return _firestore.collection(_articlePath).doc().id;
   }
 }
